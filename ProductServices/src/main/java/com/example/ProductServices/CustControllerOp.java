84e0f654af4bd3ee66ecc4bb7c718420cf24763c
NVR1999 <nusumvenkateswarareddy1999@gmail.com>
2023-11-30 17:37:01 +0530

    Nvreddy

---
+++ ProductServices/src/main/java/com/example/ProductServices/CustControllerOp.java
@@ -0,0 +1,28 @@
+package com.example.ProductServices;
+ 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class CustControllerOp {
+	
+	@RequestMapping(path="/product")
+		@GetMapping(path="/details")
+		public boolean Accountdetails(@RequestParam String accId) {
+			if(accId!=null) {
+				if(accId.equals("1")) {
+					return true;
+				}
+			}return false;
+		}
+	
+	@GetMapping(path="/moredetails")
+	public boolean AccountName(@RequestParam String name) {
+		if(name!=null) {
+			if(name.equals("a")) {
+				return true;
+			}
+		}return false;
+	}
+	}
